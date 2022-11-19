28bf2b134b0f15d33b4e730c4a19df46897f3df5
doomsday26 <harshnamdeo261999@gmail.com>
2022-11-20 00:18:23 +0530

    task1

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const http = require('http')
 const server = http.createServer((req,res)=>{
-   console.log('harsh');
+res.setHeader('Content-Type','text/html')
+res.write('<html>')   
+res.write('<head><title>First page </title>   </head>' )
+res.write('<body><h1> welcome tyo node js tutorial </h1>   </body>' )    
+res.write('</html>');
+res.end();
+
 });
 server.listen(4000);
+
 
 
