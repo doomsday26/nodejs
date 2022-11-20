b11eb9eb8738201dec57d01b17898d82f3d5aa9b
doomsday26 <harshnamdeo261999@gmail.com>
2022-11-20 13:37:49 +0530

    added all functionality

--- index.js
+++ index.js
@@ -4,15 +4,27 @@ const server = http.createServer((req,res)=>{
 const url = req.url;
 const method= req.method;
 if(url==='/'){
-    res.write('<html>');
-    res.write('<head><title>Enter Message</title></head>')
-    res.write( '<h2>write message</h2>' )
-    res.write(  '<body><form  action="/message" method="POST" ><input type="text" name="message" > <button type="submit" >send</button></form></body>'  )
+  
+  fs.readFile('response.txt', 'utf8', (err, data)=>{
+      if(err){console.log(err);}
+        console.log("fille data",data);
+
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>')
+        res.write(`<h3>${data}</h3> `)
+        res.write( '<h2>write message</h2>' )
+        res.write(  '<body><form  action="/message" method="POST" ><input type="text" name="message" > <button type="submit" >send</button></form></body>'  )
+        res.write('</html>')
+        return res.end();
+
+    });
+   
     
-    res.write('</html>')
-    return res.end();
 }
-if(url==='/message' && method==='POST'){
+
+
+
+else if(url==='/message' && method==='POST'){
 const body=[];
 req.on('data',(chunk)=>{console.log(chunk);
 body.push(chunk);
@@ -28,12 +40,15 @@ return req.on('end',()=>{
     } )
 })
 }
-res.setHeader('Content-Type','text/html');
-res.write('<html>');
-res.write('<head><title>My first page</title></head>')
-res.write(  "<body><h1>hello to project server</h1>  </body>"  )
-res.write('</html>')
-res.end();
+else{
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<head><title>My first page</title></head>')
+    res.write(  "<body><h1>hello to project server</h1>  </body>"  )
+    res.write('</html>')
+    res.end();
+}
+
 });
 server.listen(3000);
 
