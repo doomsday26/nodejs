dc562bc4f33f497f8b52872bf53b0d0014cdae40
doomsday26 <harshnamdeo261999@gmail.com>
2022-11-20 11:45:30 +0530

    taking dat and writing it in a new fime created

--- index.js
+++ index.js
@@ -1,18 +1,47 @@
 const http = require('http')
+const fs= require('fs');
 const server = http.createServer((req,res)=>{
-if(req.url==='/home'){
-    res.write('<html><h1> welcome to home</h1> </html>'   )
+const url = req.url;
+const method= req.method;
+if(url==='/'){
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title></head>')
+    res.write( '<h2>write message</h2>' )
+    res.write(  '<body><form  action="/message" method="POST" ><input type="text" name="message" > <button type="submit" >send</button></form></body>'  )
+    
+    res.write('</html>')
+    return res.end();
 }
-if(req.url==='/about'){
-    res.write('<html><h1> welcome to about us</h1> </html>'   )
-}
-if(req.url==='/node'){
-    res.write('<html><h1> welcome to node js project</h1> </html>'   )
-}
-res.end();
-
+if(url==='/message' && method==='POST'){
+const body=[];
+req.on('data',(chunk)=>{console.log(chunk);
+body.push(chunk);
+console.log("hello1");
 });
-server.listen(4000);
+req.on('end',()=>{
+    const parsebody= Buffer.concat(body).toString();
+    const msg= parsebody.split('=')[1];
+    fs.writeFileSync('response.txt',msg)
+})
+
+
+res.statusCode=302
+res.setHeader('Location','/')
+return res.end();
+
+}
+
+
+res.setHeader('Content-Type','text/html');
+res.write('<html>');
+res.write('<head><title>My first page</title></head>')
+res.write(  "<body><h1>hello to project server</h1>  </body>"  )
+
+
+res.write('</html>')
+res.end();
+});
+server.listen(3000);
 
 
 
