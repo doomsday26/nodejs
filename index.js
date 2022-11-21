4e3094c1826c79f6fdef45c5838c19cb624f05d2
doomsday26 <harshnamdeo261999@gmail.com>
2022-11-21 14:43:28 +0530

    express

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const http = require('http')
-const routes = require('./routes');
-const server = http.createServer(routes);
+const express= require ('express')
+
+
+const app= express();
+
+app.use((req,res,next)=>{
+    console.log("in middleware");
+  next(); // allows to continue to next middleware  
+})
+
+
+app.use((req,res,next)=>{
+    console.log("in the middleware");
+
+    res.send( { key1: "value" })
+
+})
+
+
+const server = http.createServer(app);
 server.listen(3000);
 
 
